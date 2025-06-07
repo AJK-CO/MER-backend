7063d3200b996b71c0792ddd127ca3716f10dbba
Ajaswanthkiran <a.jaswanthkiran@gmail.com>
2025-06-07 14:46:44 +0530

    new1

--- MER/services/fer_model.py
+++ MER/services/fer_model.py
@@ -1,9 +1,9 @@
 
 import tensorflow as tf
-
+import os
 
 # model = tf.keras.models.load_model(r"MER\services\emotion_model_final.h5")
-model = tf.keras.models.load_model(r"MER\services\emotion_recognition_model_1.h5")
+model = tf.keras.models.load_model(os.path.join("MER","services","emotion_recognition_model_1.h5"))
 
 
 def detect_emotions(face):
